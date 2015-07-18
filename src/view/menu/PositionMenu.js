1006f8d7222a75ab293a408e1277569332b76d56
Grzegorz Przybyłowicz <grzegorz.p@grzegorzp-MacBook-Pro.local>
2015-07-18 22:36:42 +0200

    postion menu bindings

--- src/view/menu/PositionMenu.js
+++ src/view/menu/PositionMenu.js
@@ -1,6 +1,6 @@
 var SubMenu = require("./SubMenu.js");
 var inherit = require("../../util").inherit;
-var msg = require("../../service").msg;
+var positionBehaviour = require("../../model").behaviourModel.positionBehaviour;
 
 function PositionMenu() {
 	SubMenu.call(this);
@@ -9,20 +9,20 @@ function PositionMenu() {
 		rows: [
 			this.checkbox("Enabled: ", {value: 0}),
 			this.section("Position: "),
-			this.positionSlider("X: ", this.emitPositionChanged),
-			this.positionSlider("Y: ", this.emitPositionChanged),
-			this.varianceSlider("Variance X: ", this.emitPositionChanged),
-			this.varianceSlider("Variance Y: ", this.emitPositionChanged),
+			this.positionSlider("position_x", "X: "),
+			this.positionSlider("position_y", "Y: "),
+			this.varianceSlider("position_variance_x", "Variance X: "),
+			this.varianceSlider("position_variance_y", "Variance Y: "),
 			this.section("Velocity: "),
-			this.positionSlider("X: "),
-			this.positionSlider("Y: "),
-			this.varianceSlider("Variance X: "),
-			this.varianceSlider("Variance Y: "),
+			this.positionSlider("velocity_x", "X: "),
+			this.positionSlider("velocity_y", "Y: "),
+			this.varianceSlider("velocity_variance_x", "Variance X: "),
+			this.varianceSlider("velocity_variance_y", "Variance Y: "),
 			this.section("Acceleration/Gravity:"),
-			this.positionSlider("X: "),
-			this.positionSlider("Y: "),
-			this.varianceSlider("Variance X: "),
-			this.varianceSlider("Variance Y: ")
+			this.positionSlider("acceleration_x", "X: "),
+			this.positionSlider("acceleration_y", "Y: "),
+			this.varianceSlider("acceleration_variance_x", "Variance X: "),
+			this.varianceSlider("acceleration_variance_y", "Variance Y: ")
 
 		]
 	};
@@ -30,39 +30,44 @@ function PositionMenu() {
 
 inherit(PositionMenu, SubMenu);
 
-PositionMenu.prototype.positionSlider = function(label, onChanged) {
+PositionMenu.prototype.positionSlider = function(id, label) {
 	var slider = this.slider(label, {
+		id: id,
 		labelWidth: 30,
 		min: -1000,
 		max: 1000,
 		value: 0
-		//on: {
-		//	onValueChanged: function() {
-		//		onChanged($$(slider.id).value);
-		//	}
-		//}
 	});
 
 	return slider;
 };
 
-PositionMenu.prototype.varianceSlider = function(label, onChanged) {
+PositionMenu.prototype.varianceSlider = function(id, label) {
 	var slider = this.slider(label, {
+		id: id,
 		labelWidth: 30,
 		max: 1000,
 		value: 0
-		//on: {
-		//	onValueChanged: function() {
-		//		onChanged($$(slider.id).value);
-		//	}
-		//}
 	});
 
 	return slider;
 };
 
-PositionMenu.prototype.emitPositionChanged = function(value) {
-	console.log(value);
+PositionMenu.prototype.onMenuCreated = function() {
+	this.bind("position_x", positionBehaviour.position, "x");
+	this.bind("position_y", positionBehaviour.position, "y");
+	this.bind("position_variance_x", positionBehaviour.positionVariance, "x");
+	this.bind("position_variance_y", positionBehaviour.positionVariance, "y");
+
+	this.bind("velocity_x", positionBehaviour.velocity, "x");
+	this.bind("velocity_y", positionBehaviour.velocity, "y");
+	this.bind("velocity_variance_x", positionBehaviour.velocityVariance, "x");
+	this.bind("velocity_variance_y", positionBehaviour.velocityVariance, "y");
+
+	this.bind("acceleration_x", positionBehaviour.acceleration, "x");
+	this.bind("acceleration_y", positionBehaviour.acceleration, "y");
+	this.bind("acceleration_variance_x", positionBehaviour.accelerationVariance, "x");
+	this.bind("acceleration_variance_y", positionBehaviour.accelerationVariance, "y");
 };
 
 module.exports = PositionMenu;
