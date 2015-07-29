eb95db9ab9da27296540a77b9d4c210ca08acabf
Grzegorz Przybyłowicz <grzegorz.p@grzegorzp-MacBook-Pro.local>
2015-07-29 16:01:16 +0200

    moveable marker test

--- src/view/ParticleView.js
+++ src/view/ParticleView.js
@@ -6,9 +6,32 @@ function ParticleView() {
 	PIXI.Container.call(this);
 
 	var image = PIXI.Sprite.fromFrame("sparkle.png");
-	image.x = 250;
-	image.y = 150;
+	image.x = 0;
+	image.y = 0;
+	image.anchor.set(0.5, 0.5);
 	image.scale.set(2, 2);
+	image.tint = 0xff0000;
+
+	image.interactive = true;
+	image.buttonMode = true;
+
+	image.mousedown = image.touchstart = function() {
+		this.dragging = true;
+	};
+
+	image.mouseup = image.mouseupoutside = image.touchend = image.touchendoutside = function(data) {
+		this.dragging = false;
+	};
+
+	image.mousemove = image.touchmove = function(event) {
+		if (this.dragging) {
+			var newPosition = event.data.getLocalPosition(this.parent);
+			this.position.x = newPosition.x;
+			this.position.y = newPosition.y;
+
+			renderer.position = this.position.clone();
+		}
+	};
 
 	this.addChild(image);
 
